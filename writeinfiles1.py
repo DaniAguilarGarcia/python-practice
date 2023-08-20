06fad3d974d75016cd4f0f8577a9876082fc8cbf
Daniela Aguilar <41087691+DaniAguilarGarcia@users.noreply.github.com>
2023-08-20 13:28:10 -0600

    aug 20 third commit

--- writeinfiles1.py
+++ writeinfiles1.py
@@ -14,3 +14,17 @@ with open(exmp2, 'r') as testwritefile:
 with open(exmp2, 'w') as writefile:
     writefile.write("This is line A\n")
     writefile.write("This is line B\n")
+
+# Check whether write to file
+
+with open(exmp2, 'r') as testwritefile:
+    print(testwritefile.read())
+
+Lines = ["This is line A\n", "This is line B\n", "This is line C\n"]
+Lines
+# Write the strings in the list to text file
+
+with open('/Example2.txt', 'w') as writefile:
+    for line in Lines:
+        print(line)
+        writefile.write(line)
