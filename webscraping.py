9f935457eb8d1d092452b269195d458729f4e096
Daniela Aguilar <41087691+DaniAguilarGarcia@users.noreply.github.com>
2023-08-20 21:17:25 -0600

    aug 20 9 commit

--- webscraping.py
+++ webscraping.py
@@ -1,1 +1,20 @@
+
+#!pip install requests
 from bs4 import BeautifulSoup
+import requests
+
+html="<!DOCTYPE html><html><head><title>Page Title</title></head><body><h3><b id='boldest'>Lebron James</b></h3><p> Salary: $ 92,000,000 </p><h3> Stephen Curry</h3><p> Salary: $85,000, 000 </p><h3> Kevin Durant </h3><p> Salary: $73,200, 000</p></body></html>"
+soup = BeautifulSoup(html, 'html5lib')
+#displays the HTML in the nested structure
+print(soup.prettify())
+
+table="<table><tr><td id='flight'>Flight No</td><td>Launch site</td> <td>Payload mass</td></tr><tr> <td>1</td><td><a href='https://en.wikipedia.org/wiki/Florida'>Florida<a></td><td>300 kg</td></tr><tr><td>2</td><td><a href='https://en.wikipedia.org/wiki/Texas'>Texas</a></td><td>94 kg</td></tr><tr><td>3</td><td><a href='https://en.wikipedia.org/wiki/Florida'>Florida<a> </td><td>80 kg</td></tr></table>"
+table_bs = BeautifulSoup(table, 'html5lib')
+table_rows=table_bs.find_all('tr')
+table_rows
+
+for i,row in enumerate(table_rows):
+    print("row",i)
+    cells=row.find_all('td')
+    for j,cell in enumerate(cells):
+        print('colunm',j,"cell",cell)
